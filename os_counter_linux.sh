9e8fcb639bada520042db49fb5cca1e175c07ecb
AKraynov <akraynov@croc.ru>
2024-08-27 11:25:45 +0300

    Update os_counter_linux.sh
    Cosmetic1

--- os_counter_linux.sh
+++ os_counter_linux.sh
@@ -34,6 +34,7 @@ sort -r     ${FN}.tmp | uniq >      ${FN}_tailed.csv
 
 
 #DF root free Mb
+
 export FN=root_free_mbytes_`/usr/bin/hostname`
 echo `date +'%Y/%m/%d %H:%M:%S'`,`df -m | grep "root"  |  awk '{print ($4)}'` >> ${FN}.csv
 #head -1     ${FN}.csv        >      ${FN}.tmp
@@ -52,6 +53,7 @@ sort -r     ${FN}.tmp | uniq >      ${FN}_tailed.csv
 
 
 #memory total RAM in OS
+
 export FN=memory_total_mbytes_`/usr/bin/hostname`
 echo `date +'%Y/%m/%d %H:%M:%S'`,`free -m | tail -2 | head -1 | awk '{print $2}'` >> ${FN}.csv
 #head -1     ${FN}.csv        >      ${FN}.tmp
@@ -61,6 +63,7 @@ sort -r     ${FN}.tmp | uniq >      ${FN}_tailed.csv
 
 
 #memory used percent
+
 export FN=memory_used_percent_`/usr/bin/hostname`
 echo `date +'%Y/%m/%d %H:%M:%S'`,`free -m | tail -2 | head -1 | awk '{print (($2-$NF)/$2)*100}'` >> ${FN}.csv
 #head -1     ${FN}.csv        >      ${FN}.tmp
@@ -72,6 +75,7 @@ sort -r     ${FN}.tmp | uniq >      ${FN}_tailed.csv
 
 
 #memory shared gbytes
+
 export FN=memory_shared_used_gb_`/usr/bin/hostname`
 echo `date +'%Y/%m/%d %H:%M:%S'`,`ipcs -m | awk '{sum += $5/1024/1024/1024} END {print sum}'` >> ${FN}.csv
 #head -1     ${FN}.csv        >      ${FN}.tmp
@@ -81,6 +85,7 @@ sort -r     ${FN}.tmp | uniq >      ${FN}_tailed.csv
 
 
 #memory shared segments count
+
 export FN=memory_shared_segmetns_count_`/usr/bin/hostname`
 echo `date +'%Y/%m/%d %H:%M:%S'`,`ipcs -m | wc -l | awk '{print $1-4}' ` >> ${FN}.csv
 #head -1     ${FN}.csv        >      ${FN}.tmp
@@ -99,6 +104,7 @@ sort -r     ${FN}.tmp | uniq >      ${FN}_tailed.csv
 
 
 #swap used mbytes
+
 export FN=swap_used_mbytes_`/usr/bin/hostname`
 echo `date +'%Y/%m/%d %H:%M:%S'`,`free -m | tail -1 | awk '{print $3}'` >> ${FN}.csv
 #head -1     ${FN}.csv        >      ${FN}.tmp
@@ -108,6 +114,7 @@ sort -r     ${FN}.tmp | uniq >      ${FN}_tailed.csv
 
 
 #semaphores allocated 
+
 export FN=semaphores_allocated`/usr/bin/hostname`
 echo `date +'%Y/%m/%d %H:%M:%S'`,`ipcs -lu | grep "allocated semaphores" | awk '{print $4}'` >> ${FN}.csv
 #head -1     ${FN}.csv        >      ${FN}.tmp
@@ -118,6 +125,7 @@ sort -r     ${FN}.tmp | uniq >      ${FN}_tailed.csv
 
 
 #free -m buff/cache
+
 export FN=free-m_buff_cache_`/usr/bin/hostname`
 echo `date +'%Y/%m/%d %k:%M:%S'`,`free -m | tail -2 | head -1 | awk '{ print ($(NF-1)) }'` >> ${FN}.csv
 #head -1     ${FN}.csv        >      ${FN}.tmp
@@ -128,4 +136,5 @@ sort -r     ${FN}.tmp | uniq >      ${FN}_tailed.csv
 #network speed current in DEV
 #ll /sys/class/net
 #while true; do old_rx=`cat /sys/class/net/eth0/statistics/rx_bytes`; sleep 10; rx=`cat /sys/class/net/eth0/statistics/rx_bytes`; echo "scale=2; (${rx}-${old_rx})/10/1024/1024" | bc; echo "`date +'"%d/%m/%Y %H:%M:%S %a"'`: Current Network speed usage Mbytes/sec"; done
+#
 
